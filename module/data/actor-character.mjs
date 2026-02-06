e7e6279dbee76718d01eba2b913ebf95c8b0788f
R.G. Wood <ric@grislyeye.com>
2026-02-06 22:56:06 +0000

    [wip] Add: basic protagonist sheet

--- module/data/actor-character.mjs
+++ module/data/actor-character.mjs
@@ -8,15 +8,14 @@ export default class GodwrotCharacter extends GodwrotActorBase {
     const schema = super.defineSchema();
 
     schema.attributes = new fields.SchemaField({
-      level: new fields.SchemaField({
-        value: new fields.NumberField({ ...requiredInteger, initial: 1 })
+      flaw: new fields.SchemaField({
+        value: new fields.StringField()
       }),
     });
 
-    // Iterate over ability names and create a new SchemaField for each.
     schema.abilities = new fields.SchemaField(Object.keys(CONFIG.GODWROT.abilities).reduce((obj, ability) => {
       obj[ability] = new fields.SchemaField({
-        value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
+        value: new fields.NumberField({ ...requiredInteger, initial: 0, min: -3, max: 3 }),
       });
       return obj;
     }, {}));
@@ -25,11 +24,8 @@ export default class GodwrotCharacter extends GodwrotActorBase {
   }
 
   prepareDerivedData() {
-    // Loop through ability scores, and add their modifiers to our sheet output.
     for (const key in this.abilities) {
-      // Calculate the modifier using d20 rules.
       this.abilities[key].mod = Math.floor((this.abilities[key].value - 10) / 2);
-      // Handle ability label localization.
       this.abilities[key].label = game.i18n.localize(CONFIG.GODWROT.abilities[key]) ?? key;
     }
   }
@@ -37,15 +33,11 @@ export default class GodwrotCharacter extends GodwrotActorBase {
   getRollData() {
     const data = {};
 
-    // Copy the ability scores to the top level, so that rolls can use
-    // formulas like `@str.mod + 4`.
     if (this.abilities) {
       for (let [k,v] of Object.entries(this.abilities)) {
         data[k] = foundry.utils.deepClone(v);
       }
     }
-
-    data.lvl = this.attributes.level.value;
 
     return data
   }
